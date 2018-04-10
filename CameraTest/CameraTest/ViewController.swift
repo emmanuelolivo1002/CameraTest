ffc3f1569cee8ed4c88005aa14263837918e6824
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-10 11:23:24 -0500

    Set up initial view and Import assets

--- CameraTest/CameraTest/ViewController.swift
+++ CameraTest/CameraTest/ViewController.swift
@@ -10,15 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var catureView: UIView!
+    @IBOutlet weak var photoModeButton: UIButton!
+    @IBOutlet weak var videoModeButton: UIButton!
+    @IBOutlet weak var toggleFlashButton: UIButton!
+    @IBOutlet weak var toggleCameraButton: UIButton!
+    @IBOutlet weak var captureButton: UIButton!
+    
+    // MARK: Variables
+    
+    // Hide status bar
+    override var prefersStatusBarHidden: Bool { return true }
+
+    
+    // MARK: Actions
+    
+    // MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCaptureButton()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Function to set button as a circle
+    func setupCaptureButton() {
+        captureButton.layer.borderColor = UIColor.black.cgColor
+        captureButton.layer.borderWidth = 2
+        
+        captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+    }
+    
+
+    
 
 
 }
